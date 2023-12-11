49072b2951f37e10d155cb375b9408cb02660120
Jarom Edwards <jaromjedwards@gmail.com>
2023-12-11 15:42:26 -0700

    a

--- final/FinalProject/Program.cs
+++ final/FinalProject/Program.cs
@@ -1,4 +1,7 @@
 using System;
+using System.IO;
+using System.Collections.Generic;
+
 
 // Abstraction: Abstract classes such as Person and MovieSurveys.
 // Encaptulation: Person encaptulates firstName, lastName, gender and age. 
@@ -10,6 +13,11 @@ class Program
 {
     static void Main(string[] args)
     {
+        // declare txt files
+        string pixarFile = "pixar.txt";
+        string disneyFile = "disney.txt";
+        string dreamworksFile = "dreamworks.txt";
+
         // Get Person Information
         Person person = new Person();
         Console.WriteLine("What is your first name?");
@@ -36,20 +44,21 @@ class Program
         }
 
         // Ask what Survey they are taking
+
         Console.WriteLine($"Welcome {firstName}! Which survey would you like to take?\n1. Pixar Movies \n2. Disney Princess Movies \n3. DreamWorks Movies");
         string userChoice = Console.ReadLine();
-        Console.WriteLine($"Survey has started. First, let's start with some general questions:");
 
-        // Get general Movie Survey responses
-        Console.WriteLine("Where do you mainly watch movies?");
-        string locationWatched = Console.ReadLine();
-        Console.WriteLine("How many hours a week do you watch movies? (Do not include hours watched TV shows, YouTube, Media content etc");
-        string hInput = Console.ReadLine();
-        Console.WriteLine("What is your prefered genre of movies?");
-        string genrePreference = Console.ReadLine();
+        // declare
+        string locationWatched = "";
+        string genrePreference = "";
+        string hInput = "";
 
-        // ask specific Pixar/Disney/Dreamworks questions
+        // PIXAR
         if (userChoice == "1"){
+
+            ProcessFile(pixarFile, firstName, lastName, gender, age);
+            askGeneralQuestions(ref locationWatched, ref genrePreference, ref hInput);
+
             Console.WriteLine($"Great! Now onto the Pixar Movie Specific questions");
 
             // Set General Movie Responses
@@ -76,8 +85,20 @@ class Program
             Console.WriteLine($"What is your least favorite Pixar movie?");
             string leastFavorite = Console.ReadLine();
 
+            // Print responses on pixar.txt
+            using (StreamWriter writer = new StreamWriter(pixarFile, true))
+            {
+                writer.WriteLine($"{firstName},{lastName},{gender},{age},{locationWatched},{genrePreference},{weeklyHoursWatched},{favorite},{leastFavorite}");
+            }
+            Console.WriteLine("Text has been written to the file: " + pixarFile);
+
         }
+        //DISNEY
         if (userChoice == "2"){
+
+            ProcessFile(disneyFile, firstName, lastName, gender, age);
+            askGeneralQuestions(ref locationWatched, ref genrePreference, ref hInput);
+
             Console.WriteLine($"Great! Now onto the Disney Movie Specific questions");
 
             // Set General Movie Responses
@@ -104,8 +125,20 @@ class Program
             Console.WriteLine($"What is your least favorite Disney movie?");
             string leastFavorite = Console.ReadLine();
 
+            // Print responses on disney.txt
+            using (StreamWriter writer = new StreamWriter(disneyFile, true))
+            {
+                writer.WriteLine($"{firstName},{lastName},{gender},{age},{locationWatched},{genrePreference},{weeklyHoursWatched},{favorite},{leastFavorite}");
+            }
+            Console.WriteLine("Text has been written to the file: " + disneyFile);
+
         }
+        // DREAMWORKS
         if (userChoice == "3"){
+
+            ProcessFile(dreamworksFile, firstName, lastName, gender, age);
+            askGeneralQuestions(ref locationWatched, ref genrePreference, ref hInput);
+
             Console.WriteLine($"Great! Now onto the Dreamworks Movie Specific questions");
 
             // Set General Movie Responses
@@ -132,7 +165,64 @@ class Program
             Console.WriteLine($"What is your least favorite Dreamworks movie?");
             string leastFavorite = Console.ReadLine();
 
+            // Print responses on dreamworks.txt
+            using (StreamWriter writer = new StreamWriter(dreamworksFile, true))
+            {
+                writer.WriteLine($"{firstName},{lastName},{gender},{age},{locationWatched},{genrePreference},{weeklyHoursWatched},{favorite},{leastFavorite}");
+            }
+            Console.WriteLine("Text has been written to the file: " + dreamworksFile);
+
+        }
+
+        static void askGeneralQuestions(ref string locationWatched, ref string genrePreference, ref string hInput){
+            Console.WriteLine($"Survey has started. First, let's start with some general questions:");
+            // Get general Movie Survey questions
+            Console.WriteLine("Where do you mainly watch movies?");
+            locationWatched = Console.ReadLine();
+
+            Console.WriteLine("What is your prefered genre of movies?");
+            genrePreference = Console.ReadLine();
+
+            Console.WriteLine("How many hours a week do you watch movies? (Do not include hours watched TV shows, YouTube, Media content etc");
+            hInput = Console.ReadLine();
+        }
+static bool ProcessFile(string filePath, string firstName, string lastName, string gender, int age)
+{
+    // Check if the file exists
+    if (File.Exists(filePath)){
+        // Read all lines from the file
+        string[] lines = File.ReadAllLines(filePath);
+
+        // Process each line
+        foreach (string line in lines)
+        {
+            // Split the line using commas as separators
+            string[] items = line.Split(',');
+
+            if (items.Length >= 4)
+            {
+                string item1 = items[0];
+                string item2 = items[1];
+                string item3 = items[2];
+
+                if (int.TryParse(items[3], out int item4))
+                {
+                    // Check for a match after parsing item4
+                    if (item1 == firstName && item2 == lastName && item3 == gender && item4 == age)
+                    {
+                        Console.WriteLine("You have already taken this survey! Please choose a different survey to take");
+                        return true;
+                    }
+                }
+            }
+            }
+            // If the loop completes without finding a match, the survey hasn't been taken
+            return false;
+            }
+            // If the file doesn't exist, the survey hasn't been taken
+            return false;
         }
 
     }
 }
+
